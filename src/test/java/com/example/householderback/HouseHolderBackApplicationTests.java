8f2a78a44dbc352438a9fb0b76d0def5b59fa122
lhz <2190732276@qq.com>
2022-04-12 10:49:45 +0800

    用户注册登录

---
+++ src/test/java/com/example/householderback/HouseHolderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.householderback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHolderBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
